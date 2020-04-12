654cce1a1abd865bafef2d2dfb11182cce2053e3
johnwing <47427803+johnwing@users.noreply.github.com>
2020-04-13 03:47:13 +0800

    install fire

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//import firebase from "firebase";
+
+
 // use express method
 var express = require('express');
 var app = express();
-
+var firebase = require("firebase");
 var mysql = require('mysql');
 var engine = require('ejs-locals');
 const bodyParser = require('body-parser');
@@ -14,11 +17,49 @@ app.set('views', './views');
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+const Firestore = require('@google-cloud/firestore');
+const firestore = new Firestore({
+    projectId: 'cuhkfyp2020'
+});
+
+//firebase setup
+//https://www.oxxostudio.tw/articles/201905/firebase-firestore.html
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDfdXMCk4_oo5UGkiA20IYoPGE6sKvstzk",
+    authDomain: "cuhkfyp2020.firebaseapp.com",
+    databaseURL: "https://cuhkfyp2020.firebaseio.com",
+    projectId: "cuhkfyp2020",
+    storageBucket: "cuhkfyp2020.appspot.com",
+    messagingSenderId: "12732194759",
+    appId: "1:12732194759:web:c18707b920f409bde0c3e2",
+    measurementId: "G-TM10H85S0Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+var db = firebase.firestore();
+var ref = db.collection('fruit'); // add() 是針對集合使用
+
+
+
+
+
 
 // use express get method 
 // create root router and print hello world
 app.get('/', function(req, res){
   res.render('index.html');
+  ref.add({
+  total:450,
+  good:400
+}).then(() => {
+  console.log('add data successful');
+});
+
+
 });
 
 // check running enviroment
