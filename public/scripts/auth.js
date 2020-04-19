97801e0ea47ee34bb0d5d38a2fb2f542552cd11a
johnwing <47427803+johnwing@users.noreply.github.com>
2020-04-19 18:50:12 +0800

    google login

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -25,6 +25,42 @@ signupForm.addEventListener('submit', (e) => {
   });
 });
 
+
+// google sign up
+const googleSignupForm = document.querySelector('#google-signup-form');
+googleSignupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("googleLogin");
+
+  //sign in by redirecting to the sign-in page
+  auth.signInWithRedirect(provider);
+  auth.getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+    console.log(token);
+  }
+  // The signed-in user info.
+  var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+
+    console.log(errorMessage);
+  });
+
+
+});
+
+
+
 // logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
