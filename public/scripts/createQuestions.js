5c843b9bdc48d07ac9872fb77a73f63b1806b066
johnwing <47427803+johnwing@users.noreply.github.com>
2020-06-07 16:45:50 +0800

    finnintheblank

--- public/scripts/createQuestions.js
+++ public/scripts/createQuestions.js
@@ -28,6 +28,7 @@ const setupUI=(user) => {
 
 $('#questionType').on("change",function()
 {
+	//$('#questionType').html("");
 	var questionType=$(this).val();
 	//$(".questions").css("display", "none");
 	console.log(questionType);
@@ -38,7 +39,7 @@ $('#questionType').on("change",function()
 		answerFilterContent='<div class="mc form-check questions" id="mc" style="display:block"> <p>Instructions:</p> <p>Tick the box(es) that gives the marks. Write down the represent answer of the choice next to the checkbox.</p> Number of Choice: <select class="form-control" id="noOfmcChoice"> <option disabled selected="selected">--Number of Choice--</option><option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> <option value="6">6</option> <option value="7">7</option> </select> <div id="mcChoiceList"> </div> </div>';
 	}else if(questionType=="fillInTheBlank")
 	{
-		answerFilterContent='';
+		answerFilterContent='<div class="fillInTheBlank form-check questions" id="fillInTheBlank" style="display:block"> <p>Instructions:</p> <p>Write down the possible answer(s) below for auto-marking. If you do not need auto-marking, select "0" in the number of possible solution.</p> Number of the Possible Answer(s): <select class="form-control" id="noOfFillInTheBlankChoice"> <option value="0">0</option> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> <option value="6">6</option> <option value="7">7</option> </select> <div id="fillInTheBlankList"> </div> </div>';
 	}
 	else if(questionType=="shortQ")
 	{
@@ -66,6 +67,20 @@ $('#answerFilter').on("change",'#noOfmcChoice',function()
 	{
 		var answerChar=String.fromCharCode(i+65);
 		$('#mcChoiceList').append('<p><input type="checkbox"  value="'+i+'" class="mcAnswer" > '+answerChar+') <input type="text" class="radioInputAnswer id='+i+'"></p>');
+	}
+	console.log("ok");
+})
+
+
+$('#answerFilter').on("change",'#noOfFillInTheBlankChoice',function()
+{
+	$('#mcChoiceList').html("");
+	var i=0;
+	var n=parseInt($('#noOfFillInTheBlankChoice').val());
+	//alert(n);
+	for(i=0;i<n;i++)
+	{
+		$('#mcChoiceList').append('<p><input type="text" class="radioInputAnswer id='+i+'"></p>');
 	}
 	console.log("ok");
 })
