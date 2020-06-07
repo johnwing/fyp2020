c15fcad84b011bfb1c6a33ff714ecd976cc5e202
johnwing <47427803+johnwing@users.noreply.github.com>
2020-06-07 16:12:12 +0800

    mc answer

---
+++ public/scripts/createQuestions.js
@@ -0,0 +1,86 @@
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    user.getIdTokenResult().then(idTokenResult => {
+      user.admin = idTokenResult.claims.admin;
+      setupUI(user);
+    });
+    /*db.collection('fileLink').onSnapshot(snapshot => {
+      setupGuides(snapshot.docs);
+    }, err => console.log(err.message));*/
+  } else {
+    setupUI();
+    //setupGuides([]);
+  }
+});
+
+
+
+var userUid;
+const setupUI=(user) => {
+
+	console.log(user.uid);
+	userUid=user.uid;
+}
+
+
+
+
+$('#questionType').on("change",function()
+{
+	var questionType=$(this).val();
+	//$(".questions").css("display", "none");
+	console.log(questionType);
+	var answerFilterContent='';
+
+	if(questionType=="mc")
+	{
+		answerFilterContent='<div class="mc form-check questions" id="mc" style="display:block"> <p>Instructions:</p> <p>Tick the box(es) that gives the marks. Write down the represent answer of the choice next to the checkbox.</p> Number of Choice: <select class="form-control" id="noOfmcChoice"> <option disabled selected="selected">--Number of Choice--</option><option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> <option value="6">6</option> <option value="7">7</option> </select> <div id="mcChoiceList"> </div> </div>';
+	}else if(questionType=="fillInTheBlank")
+	{
+		answerFilterContent='';
+	}
+	else if(questionType=="shortQ")
+	{
+
+	}
+	else if(questionType=="longQ")
+	{
+
+	}
+	$('#answerFilter').html(answerFilterContent);
+
+
+	
+});
+
+
+
+$('#answerFilter').on("change",'#noOfmcChoice',function()
+{
+	$('#mcChoiceList').html("");
+	var i=0;
+	var n=parseInt($('#noOfmcChoice').val());
+	//alert(n);
+	for(i=0;i<n;i++)
+	{
+		var answerChar=String.fromCharCode(i+65);
+		$('#mcChoiceList').append('<p><input type="checkbox"  value="'+i+'" class="mcAnswer" > '+answerChar+') <input type="text" class="radioInputAnswer id='+i+'"></p>');
+	}
+	console.log("ok");
+})
+
+$('#answerFilter').on("change",'.mcAnswer',function()
+{
+	var selectedValue=[];
+	$('.mcAnswer:checked').each(function()
+	{
+		selectedValue.push($(this).val());
+	});
+	console.log(selectedValue);
+
+})
+
+
+
+
---
+++ public/scripts/initFirebase.js
@@ -0,0 +1,20 @@
+    var config = {
+      apiKey: "AIzaSyDfdXMCk4_oo5UGkiA20IYoPGE6sKvstzk",
+      authDomain: "cuhkfyp2020.firebaseapp.com",
+      databaseURL: "https://cuhkfyp2020.firebaseio.com",
+      projectId: "cuhkfyp2020",
+      storageBucket: "cuhkfyp2020.appspot.com",
+      messagingSenderId: "12732194759",
+      appId: "1:12732194759:web:c18707b920f409bde0c3e2",
+      measurementId: "G-TM10H85S0Y"
+  };
+    firebase.initializeApp(config);
+    
+    // make auth and firestore references
+    const auth = firebase.auth();
+    const db = firebase.firestore();
+    const functions = firebase.functions();
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    // update firestore settings
+    db.settings({ timestampsInSnapshots: true });
